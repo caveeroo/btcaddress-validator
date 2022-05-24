91faefcf64a0827fb4250729d615a91b61b23170
caveeroo <62477582+caveeroo@users.noreply.github.com>
2022-05-24 18:59:23 +0200

    Abuse report print option added

--- btcaddressvalidator.sh
+++ btcaddressvalidator.sh
@@ -1,15 +1,42 @@
 #/bin/bash
 
-while getopts u:a:f: flag
-do
-    case "${flag}" in
-        a) btcaddress=${OPTARG};;
-    esac
+show_help () {
+        echo "Btc address validator options: "
+        echo
+        echo "-a | --address    Provide a btc address to analyze"
+        echo "-r | --report     Provide an abuse report of the specified address"
+}
+
+while :; do
+        case "$1" in
+                -h|-\?|--help)
+                        show_help
+                        exit
+                        ;;
+                -a|--address)
+                        if [ "$2" ]; then
+                                btcaddress=$2
+                                shift
+                        else
+                                die 'ERROR: "--address" requires a non-empty address.'
+                        fi
+                        shift
+                        ;;
+                -r|--report)
+                        abusereport="true"
+                        shift
+                        ;;
+                *)
+                        break
+        esac
 done
+
 if [ -z "$btcaddress" ]
 then
         read -p 'Btc address: ' btcaddress
 fi
+
+bold=$(tput bold)
 
 dir="$(mktemp)"
 
@@ -33,6 +60,27 @@ echo "$isitvalid"
 if [ "$isitvalid" = "Valid Bitcoin Address" ]; then
         echo "Address type: $hashtype"
         echo "Address info: $linkblockchain"
+        if [ "$abusereport" = "true" ]; then
+                echo
+                echo "Checking abuse reports"
+                curl https://www.bitcoinabuse.com/reports/112FWGSL2q7rVTgabQuJbo3WwKid8dMEtj -o $dir/abusereport -s
+                grep -oP '<td>.+' $dir/abusereport > $dir/relevantinfo
+                reportcount=$(sed -n 2p $dir/relevantinfo | grep -oP 'td>\K\d+')
+                echo
+                echo '\033[1mThis address has been reported '"$reportcount" 'times\033[0m'
+                echo "Printing all reports..."
+                echo
+                j=1
+                for i in $(seq 4 $((($reportcount*3)+3)))
+                do
+                        if [ $((($i-4)%3)) = 0 ];then
+                                echo "Report n$j"
+                                j=$(($j+1))
+                        fi
+                        sed -n $(echo $i)p $dir/relevantinfo | sed -e 's/<[^>]*>//g'
+                        echo
+                done
+        fi
 fi
 
 rm -rf $dir
